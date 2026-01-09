2b2b1a9c1f4059d279fcffe410192747a4743b50
Rhoward1967 <GENERALMANAGER81@GMAIL.COM>
2026-01-08 21:40:35 -0500

    Add npm-free server startup script for F: drive independence

---
+++ start-server.cmd
@@ -0,0 +1,6 @@
+@echo off
+REM ODYSSEY-1 Server Starter (No npm required)
+REM Works even with F: drive disconnected
+
+echo Starting ODYSSEY-1 Server...
+node .\node_modules\vite\bin\vite.js --port 8080
